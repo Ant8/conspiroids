88a593d39f016a966d482426b4f0cd7deed6021d
Antoni Moroz <antek@codequest.com>
2020-05-17 20:00:30 +0200

    initial project

---
+++ desktop/src/com/abm/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.abm.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.abm.game.ConspiroidsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConspiroidsGame(), config);
+	}
+}
---
+++ ios/src/com/abm/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.abm.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.abm.game.ConspiroidsGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new ConspiroidsGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
